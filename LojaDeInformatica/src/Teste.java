5be60cdc8ef9f4fc115e971ed10d6fb3ff30c894
Ronaldo Mendonça Zica <rzica42@gmail.com>
2020-09-01 14:01:16 -0300

    Adicionada versão inicial do código.
    Adicionada versão inicial do código.
    Contém também primeira versão da UML.

---
+++ LojaDeInformatica/src/Teste.java
@@ -0,0 +1,15 @@
+
+/*
+ * Autor do código:
+ *      Ronaldo Mendonça Zica
+ *      E-mail: rzica42@gmail.com
+ *      Github: ronaldozica
+ */
+
+public class Teste {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
